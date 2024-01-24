19e33000755dda73d066540747a2fe4b6297008a
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2024-01-24 15:34:32 +0000

    set permissons only if needed

--- setup.sh
+++ setup.sh
@@ -26,60 +26,35 @@ echo_message() {
 
 # Function to copy and set permissions for SSH key files
 set_permissions() {
-    local key_name="$1"
-    local key_path="$ssh_dir/$key_name"
-    local pub_key_path="$ssh_dir/$key_name.pub"
+    local file_path="$1"
+    local desired_permissions="$2"
 
-    if [ -f "$key_path" ]; then
-        chmod 600 "$key_path"
-        echo_success "permissions set for $key_path"
-    else
-        echo_warning "$key_name not found at $key_path - Please copy it to $ssh_dir and run this script again."
-    fi
+    # Check if the file exists
+    if [ -e "$file_path" ]; then
+        # Check current permissions
+        current_permissions=$(stat -c "%a" "$file_path")
 
-    if [ -f "$pub_key_path" ]; then
-        chmod 644 "$pub_key_path"
-        echo_success "permissions set for $pub_key_path"
+        # Check if permissions are different from the desired value
+        if [ "$current_permissions" != "$desired_permissions" ]; then
+            # Set permissions to the desired value
+            chmod "$desired_permissions" "$file_path"
+            echo_success "permissions for $file_path set to $desired_permissions."
+        else
+            echo_message "permissions for $file_path are already $desired_permissions."
+        fi
     else
-        echo_warning "$key_name.pub not found at $pub_key_path - Please copy it to $ssh_dir and run this script again."
+        echo "Error: $file_path does not exist."
     fi
 }
-
-# Install Zsh if not already installed
-if ! command -v zsh &>/dev/null; then
-    sudo apt update
-    sudo apt install -y zsh
-    chsh -s $(which zsh)
-    echo_success "Zsh installed and configured."
-else
-    echo_message "Zsh is already installed."
-fi
-
-# Check if Homebrew is installed
-if ! command -v brew &>/dev/null; then
-    # Install Homebrew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    # Add Homebrew to Zsh configuration
-    echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.zshrc
-    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-
-    # Test Homebrew installation
-    brew doctor
-
-    echo_success "Homebrew installed and configured."
-else
-    echo_message "Homebrew is already installed."
-fi
 
 # Set up SSH directory if not already present
 ssh_dir="$HOME/.ssh"
 if [ ! -d "$ssh_dir" ]; then
     mkdir -p "$ssh_dir"
     chmod 700 "$ssh_dir"
-    echo_success "SSH directory created."
+    echo_success "~/.ssh directory created."
 else
-    echo_message "SSH directory already exists."
+    echo_message "directory ~/.ssh already exists."
 fi
 
 # Configure SSH config file for GitHub and GitLab
@@ -107,17 +82,47 @@ else
     echo_message "Azure DevOps entry already exists in $config_file."
 fi
 
-chmod 600 "$config_file"
-echo_success "SSH config file configured and permissions set."
+set_permissions "$config_file" 600
+echo_success "~/.ssh/config file configured and permissions set."
 
 # Copy and set permissions for GitHub SSH key
-set_permissions "id_ed25519_github"
+set_permissions "~/.ssh/id_ed25519_github" 600
+set_permissions "~/.ssh/id_ed25519_github.pub" 644
 
 # Copy and set permissions for GitLab SSH key
-set_permissions "id_ed25519_gitlab"
+set_permissions "~/.ssh/id_ed25519_gitlab" 600
+set_permissions "~/.ssh/id_ed25519_gitlab.pub" 644
 
 # Copy and set permissions for Azure DevOps SSH key
-set_permissions "id_rsa_azuredevops"
+set_permissions "~/.ssh/id_rsa_azuredevops" 600
+set_permissions "~/.ssh/id_rsa_azuredevops.pub" 644
+
+# Install Zsh if not already installed
+if ! command -v zsh &>/dev/null; then
+    sudo apt update
+    sudo apt install -y zsh
+    chsh -s $(which zsh)
+    echo_success "zsh installed and configured."
+else
+    echo_message "zsh is already installed."
+fi
+
+# Check if Homebrew is installed
+if ! command -v brew &>/dev/null; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Add Homebrew to Zsh configuration
+    echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.zshrc
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+    # Test Homebrew installation
+    brew doctor
+
+    echo_success "Homebrew installed and configured."
+else
+    echo_message "Homebrew is already installed."
+fi
 
 echo_message "chezmoi..."
 if ! command -v chezmoi &>/dev/null; then
