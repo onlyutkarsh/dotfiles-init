37215c2f88f5ad4c5322b8caccf2b65f9d305b5b
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2024-01-24 12:06:08 +0000

    setup permissions and install chezmoi and git

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # info
 
 initial script to run to setup zsh and brew on a new WSL
+
+```shell
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/onlyutkarsh/dotfiles-init/main/setup.sh)"
+```
--- setup.sh
+++ setup.sh
@@ -1,20 +1,88 @@
 #!/bin/bash
 
-# Install Zsh
-sudo apt update
-sudo apt install -y zsh
+# Install Zsh if not already installed
+if ! command -v zsh &>/dev/null; then
+    sudo apt update
+    sudo apt install -y zsh
+    chsh -s $(which zsh)
+else
+    echo "Zsh is already installed."
+fi
 
-# Set Zsh as the default shell
-chsh -s $(which zsh)
+# Check if Homebrew is installed
+if ! command -v brew &>/dev/null; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # Add Homebrew to Zsh configuration
+    echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.zshrc
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-# Add Homebrew to Zsh configuration
-echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.zshrc
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    # Test Homebrew installation
+    brew doctor
 
-# Test Homebrew installation
-brew doctor
+    echo "Homebrew installed and configured."
+else
+    echo "Homebrew is already installed."
+fi
 
-echo "Zsh and Homebrew setup complete!"
+echo "Zsh and Homebrew setup complete! Setting up SSH directory now..."
+
+# Set up SSH directory if not already present
+ssh_dir="$HOME/.ssh"
+if [ ! -d "$ssh_dir" ]; then
+    mkdir -p "$ssh_dir"
+    chmod 700 "$ssh_dir"
+fi
+
+# Configure SSH config file for GitHub and GitLab
+cat <<EOF >>"$ssh_dir/config"
+
+Host github.com
+    HostName github.com
+    IdentityFile $ssh_dir/id_ed25519_github
+
+Host gitlab.com
+    HostName gitlab.com
+    IdentityFile $ssh_dir/id_ed25519_gitlab
+EOF
+chmod 600 "$ssh_dir/config"
+echo "SSH config file configured."
+
+# Function to copy and set permissions for SSH key files
+set_permissions() {
+    local key_name="$1"
+    local key_path="$ssh_dir/$key_name"
+    local pub_key_path="$ssh_dir/$key_path.pub"
+
+    if [ -f "$key_path" ] && [ -f "$pub_key_path" ]; then
+        chmod 600 "$ssh_dir/$key_name"
+        chmod 644 "$ssh_dir/$key_name.pub"
+        echo "$key_name and $key_name.pub permissions set."
+    else
+        echo "Error: $key_name or $key_name.pub not found at $key_path - Please copy them to $ssh_dir and run this script again."
+    fi
+}
+
+# Copy and set permissions for GitHub SSH key
+set_permissions "id_ed25519_github"
+
+# Copy and set permissions for GitLab SSH key
+set_permissions "id_ed25519_gitlab"
+
+echo "SSH directory setup complete! Setting up Chezmoi now..."
+if ! command -v chezmoi &>/dev/null; then
+    brew install chezmoi
+fi
+
+echo "update git using brew"
+# Check if Git is installed
+if brew list --formula | grep -q "git"; then
+    # Update Git using Homebrew
+    brew upgrade git
+    echo "Git updated using Homebrew."
+else
+    # Install Git using Homebrew
+    brew install git
+    echo "Git installed using Homebrew."
+fi
